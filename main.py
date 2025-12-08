8b121d5807ed9048bbfab238c3b1ea97908ac0fa
Mojiboop <git@moji.rip>
2025-12-08 15:59:28 +0000

    Added clock object

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt =0
+    clock = pygame.time.Clock()
     while True:
         log_state()
         for event in pygame.event.get():
@@ -14,10 +16,8 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
-
-  #  print(f"Starting Asteroids with pygame version: (%s)" % pygame.version.ver)
- #   print(f"Screen width: {SCREEN_WIDTH}")
-#    print(f"Screen height: {SCREEN_HEIGHT}")
+        clock.tick(60)
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
