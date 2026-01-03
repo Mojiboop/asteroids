a6ac8908eed395e071520eeefaedc7130596c533
Mojiboop <git@moji.rip>
2026-01-03 16:39:09 +0000

    Added rotation. CH3 complete

--- main.py
+++ main.py
@@ -2,19 +2,23 @@ import pygame
 
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     dt =0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     clock = pygame.time.Clock()
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
         screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
+
         pygame.display.flip()
         clock.tick(60)
         dt = clock.tick(60)/1000
